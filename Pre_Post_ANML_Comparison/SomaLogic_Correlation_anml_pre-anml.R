65282767e5fe77a90b2bf4ad2c4ad8a35646f92e
wanghe98 <103050210+wanghe98@users.noreply.github.com>
2025-08-22 18:34:40 +0800

    Update SomaLogic_Correlation_anml_pre-anml.R

--- Pre_Post_ANML_Comparison/SomaLogic_Correlation_anml_pre-anml.R
+++ Pre_Post_ANML_Comparison/SomaLogic_Correlation_anml_pre-anml.R
@@ -3,14 +3,14 @@
 #ANML
 
 #SomaLogic Data
-Dat <- read.csv("D:/PhD Thesis/Multi-Platform Proteomics Comparison/1_Preprocessing/1_SomaLogic_anml_Preprocessing/Somalogic_Merged_All.csv")
+Dat <- read.csv("Somalogic_Merged_All.csv")
 rownames(Dat) <- Dat$UniqueID
 Label <- Dat[,c(1:20)]
 ProtMatrix <- Dat[,c(21:ncol(Dat))]
 rm(Dat)
 
 #Analyte Annotation
-Analyte_Annotation <- read.csv("D:/PhD Thesis/Multi-Platform Proteomics Comparison/1_Preprocessing/1_SomaLogic_anml_Preprocessing/Somalogic_Analyte_Annotation_All.csv")
+Analyte_Annotation <- read.csv("Somalogic_Analyte_Annotation_All.csv")
 rownames(Analyte_Annotation) <- Analyte_Annotation$AptName
 
 #Remove Control Samples
@@ -30,14 +30,14 @@ rm(Index)
 #Pre-ANML
 
 #SomaLogic Data
-Dat <- read.csv("D:/PhD Thesis/Multi-Platform Proteomics Comparison/1_Preprocessing/2_SomaLogic_pre-anml_Preprocessing/Somalogic_Merged_All.csv")
+Dat <- read.csv("SomaLogic_pre-anml_Preprocessing/Somalogic_Merged_All.csv")
 rownames(Dat) <- Dat$UniqueID
 Label2 <- Dat[,c(1:20)]
 ProtMatrix2 <- Dat[,c(21:ncol(Dat))]
 rm(Dat)
 
 #Analyte Annotation
-Analyte_Annotation2 <- read.csv("D:/PhD Thesis/Multi-Platform Proteomics Comparison/1_Preprocessing/2_SomaLogic_pre-anml_Preprocessing/Somalogic_Analyte_Annotation_All.csv")
+Analyte_Annotation2 <- read.csv("SomaLogic_pre-anml_Preprocessing/Somalogic_Analyte_Annotation_All.csv")
 rownames(Analyte_Annotation2) <- Analyte_Annotation2$AptName
 
 #Remove Control Samples
@@ -59,12 +59,12 @@ Label2 <- Label2[rownames(Label), colnames(Label)]
 Analyte_Annotation2 <- Analyte_Annotation2[rownames(Analyte_Annotation), colnames(Analyte_Annotation)]
 ProtMatrix2 <- ProtMatrix2[rownames(ProtMatrix), colnames(ProtMatrix)]
 
-#Pearson Correlation
+#Spearman Correlation
 
 #Protein-Level
 Protein_Cor <- vector()
 for (i in 1:10675) {
-  Protein_Cor[i] <- cor(ProtMatrix[,i], ProtMatrix2[,i])
+  Protein_Cor[i] <- cor(ProtMatrix[,i], ProtMatrix2[,i], method = "spearman")
 }
 rm(i)
 summary(Protein_Cor)
@@ -79,14 +79,14 @@ ggplot(data, aes(x = correlation, fill = group)) +
   geom_histogram(binwidth = 0.05, color = "black") +
   theme_classic() +
   scale_fill_manual(values = c("< 0.6" = "#0072B5FF", "0.6 - 0.8" = "#E18727FF", "> 0.8" = "#BC3C29FF")) +
-  labs(title = "SomaLogic Protein-level", x = "Pearson correlation between pre-ANML and ANML", y = "Frequency", fill = "Correlation Range") +
+  labs(title = "SomaLogic Protein-level", x = "Spearman correlation between pre-ANML and ANML", y = "Frequency", fill = "Correlation Range") +
   theme(axis.title.x = element_text(size = 15), axis.title.y = element_text(size = 15)) +
   theme(axis.text.x = element_text(size = 14, color = "black"), axis.text.y = element_text(size = 14, color = "black")) +
   theme(plot.title = element_text(size = 15, face = "bold")) +
   theme(plot.title = element_text(hjust = 0.1))
 rm(data)
 
-write.csv(data.frame(Protein = colnames(ProtMatrix), Corr = Protein_Cor), "Protein_Level_Pearson_Correlation_anml_pre-anml.csv", row.names = FALSE)
+write.csv(data.frame(Protein = colnames(ProtMatrix), Corr = Protein_Cor), "Protein_Level_Spearman_Correlation_anml_pre-anml.csv", row.names = FALSE)
 
 rm(ProtMatrix)
 rm(ProtMatrix2)
@@ -97,3 +97,4 @@ rm(Analyte_Annotation2)
 
 rm(Protein_Cor)
 gc()
+
